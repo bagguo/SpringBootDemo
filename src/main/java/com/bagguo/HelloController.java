a6afca5084599420e4161e1d80773b2e60128211
dai <dai12.gmail.com>
2022-05-05 16:54:32 +0800

    hello spring boot

---
+++ src/main/java/com/bagguo/HelloController.java
@@ -0,0 +1,14 @@
+package com.bagguo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        System.out.println("hello spring boot demo");
+        return "hello spring boot demo";
+    }
+}
